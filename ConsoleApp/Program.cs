bbf38c5fda82788c3f637ed68906abdf979ab1b9
Aliaksei Liakhovich <s32909@pjwstk.edu.pl>
2026-03-10 13:31:22 +0100

    Completed user interaction

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,3 +9,19 @@ for (int i = 0; i < n; i++)
 {
     arr[i] = InputHandler.AskForANumber("");
 }
+
+Action:
+char act = InputHandler.AskForAChar("Calculate the average(a) or the median(m)?: ");
+
+switch (act)
+{
+    case 'a':
+        Console.WriteLine("The average is: {0}", StatisticsHelper.CalculateAverage(arr));
+        break;
+    case 'm':
+        Console.WriteLine("The median is: {0}", StatisticsHelper.CalculateMedian(arr));
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        goto Action;
+}
