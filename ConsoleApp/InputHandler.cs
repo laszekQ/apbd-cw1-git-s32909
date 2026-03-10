719cbafee38169e817071cef5b3d828a726ff49d
Aliaksei Liakhovich <s32909@pjwstk.edu.pl>
2026-03-10 13:21:26 +0100

    Exception Handling in InputHandler

--- ConsoleApp/InputHandler.cs
+++ ConsoleApp/InputHandler.cs
@@ -5,6 +5,36 @@ public static class InputHandler
     public static double AskForANumber(String msg)
     {
         Console.Write(msg);
-        return double.Parse(Console.ReadLine());
+
+        double input = 0.0;
+        try
+        {
+            input = double.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.Write("Please enter the number in a valid format: ");
+            return AskForANumber("");
+        }
+        return input;
+    }
+
+    public static char AskForAChar(String msg)
+    {
+        Console.Write(msg);
+
+        char input = '\0';
+
+        try
+        {
+            char.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.Write("Please enter a single character: ");
+            return AskForAChar("");
+        }
+
+        return input;
     }
 }
